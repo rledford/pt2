dc7f1b8b9476e09268090ede41a4e5800280ec55
rledford <ryan.c.ledford@gmail.com>
2017-12-14 14:46:32 -0600

    refined source and updated test - replaced es6 const and let keywords with var

--- index.js
+++ index.js
@@ -1,143 +1,104 @@
-const DEG2RAD = 0.01745329252;
-
+var DEG2RAD = 0.01745329252;
 class Point {
-  constructor(coords = { x: 0, y: 0 }) {
-    this.x = coords.x || 0;
-    this.y = coords.y || 0;
+  constructor(point = { x: 0, y: 0 }) {
+    this.x = point.x;
+    this.y = point.y;
   }
-
   set(point = { x: 0, y: 0 }) {
     this.x = point.x;
     this.y = point.y;
-
     return this;
   }
-
   clone() {
     return new Point({ x: this.x, y: this.y });
   }
-
   add(point = { x: 0, y: 0 }) {
-    this.x += point.x || 0;
-    this.y += point.y || 0;
-
+    this.x += point.x;
+    this.y += point.y;
     return this;
   }
-
   sub(point = { x: 0, y: 0 }) {
-    this.x -= point.x || 0;
-    this.y -= point.y || 0;
-
+    this.x -= point.x;
+    this.y -= point.y;
     return this;
   }
-
   scale(value) {
     this.x *= value;
     this.y *= value;
-
     return this;
   }
-
   scaled(value) {
     return this.clone().scale(value);
   }
-
   cross(point = { x: 0, y: 0 }) {
     return this.x * point.y - this.y * point.x;
   }
-
   dot(point = { x: 0, y: 0 }) {
     return this.x * point.x + this.y * point.y;
   }
-
   mag2() {
     return Math.pow(this.x, 2) + Math.pow(this.y, 2);
   }
-
   mag() {
     return Math.sqrt(this.mag2());
   }
-
   rotate(degrees, origin = { x: 0, y: 0 }) {
-    let angle = DEG2RAD * degrees;
-    let s = Math.sin(angle);
-    let c = Math.cos(angle);
+    var angle = DEG2RAD * degrees;
+    var s = Math.sin(angle);
+    var c = Math.cos(angle);
     this.sub(origin);
-    let x = this.x * c - this.y * s;
-    let y = this.x * s + this.y * c;
+    var x = this.x * c - this.y * s;
+    var y = this.x * s + this.y * c;
     this.set({ x: x, y: y });
     this.add(origin);
-
     return this;
   }
-
   rotated(degrees, origin = { x: 0, y: 0 }) {
     return this.clone().rotate(degrees, origin);
   }
-
   normalize() {
-    let mag = this.mag();
+    var mag = this.mag();
     if (mag === 0) return this;
-
     this.x = this.x / mag;
     this.y = this.y / mag;
-
     return this;
   }
-
   normalized() {
     return this.clone().normalize();
   }
-
   negate() {
-    this.x *= -1;
-    this.y *= -1;
-
-    return this;
+    return this.scale(-1);
   }
-
   negated() {
-    return this.clone().negate();
+    return this.clone().scale(-1);
   }
-
   dist2(point = { x: 0, y: 0 }) {
-    let dx = (point.x || 0) - this.x;
-    let dy = (point.y || 0) - this.y;
-
+    var dx = point.x - this.x;
+    var dy = point.y - this.y;
     return Math.pow(dx, 2) + Math.pow(dy, 2);
   }
-
   dist(point = { x: 0, y: 0 }) {
     return Math.sqrt(this.dist2(point));
   }
-
   radiansTo(point = { x: 0, y: 0 }) {
     return Math.atan2(point.y - this.y, point.x - this.x);
   }
-
   radiansFrom(point = { x: 0, y: 0 }) {
     return Math.atan2(this.y - point.y, this.x - point.x);
   }
-
   degreesTo(point = { x: 0, y: 0 }) {
-    let degrees = this.radiansTo(point) / DEG2RAD;
+    var degrees = this.radiansTo(point) / DEG2RAD;
     return degrees < 0 ? degrees + 360 : degrees;
   }
-
   degreesFrom(point = { x: 0, y: 0 }) {
     return new Point(point).degreesTo(this);
   }
-
   directionTo(point = { x: 0, y: 0 }) {
-    let pt = new Point({ x: point.x - this.x, y: point.y - this.y });
-    pt.normalize();
-
-    return pt;
+    var pt = new Point({ x: point.x - this.x, y: point.y - this.y });
+    return pt.normalize();
   }
-
   directionFrom(point = { x: 0, y: 0 }) {
     return this.directionTo(point).negate();
   }
 }
-
 module.exports = Point;
--- test.js
+++ test.js
@@ -1,13 +1,10 @@
 var Point = require('./index');
-
 function testGroup1() {
   var methods = [
     'add',
     'sub',
     'cross',
     'dot',
-    'normalize',
-    'normalized',
     'dist',
     'dist2',
     'radiansTo',
@@ -29,7 +26,6 @@ function testGroup1() {
     console.log('***************************');
   });
 }
-
 function testGroup2() {
   var methods = ['scale', 'scaled'];
   methods.forEach(function(method) {
@@ -44,13 +40,23 @@ function testGroup2() {
     console.log('***************************');
   });
 }
-
 function testGroup3() {
+  var methods = ['normalize', 'normalized', 'negate', 'negated'];
+  methods.forEach(function(method) {
+    var pointA = new Point({ x: 1, y: -1 });
+    console.log('***************************');
+    console.log('test', method);
+    console.log('pointA', pointA);
+    console.log(pointA[method]());
+    console.log('***************************');
+  });
+}
+function testGroup4() {
   var pointA = new Point({ x: 1, y: 1 });
   var origin = new Point({ x: 2, y: 1 });
   var degrees = 180;
   console.log('***************************');
-  console.log('test rotations');
+  console.log('test rotation');
   console.log('pointA', pointA);
   console.log('degrees', degrees);
   console.log('origin', origin);
@@ -62,8 +68,9 @@ function testGroup3() {
   console.log(pointA.clone().rotate(degrees, origin));
   console.log('rotated - with origin');
   console.log(pointA.clone().rotated(degrees, origin));
+  console.log('***************************');
 }
-
 testGroup1();
 testGroup2();
 testGroup3();
+testGroup4();
